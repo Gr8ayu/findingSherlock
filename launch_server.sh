7f5f4596eb600dcc548018a471a771bbb0c87587
gr8ayu <anonymouskmr@gmail.com>
2020-02-25 11:41:40 +0530

    for deployment testing

--- launch_server.sh
+++ launch_server.sh
@@ -2,4 +2,5 @@ cd ~/Gr8ayu/website/relay-hack/
 pwd
 conda deactivate
 source relay-hack-venv/bin/activate
-python3 mysite/manage.py runserver
+python3 mysite/manage.py runserver --insecure 0.0.0.0:8000
+
